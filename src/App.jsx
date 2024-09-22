4903165089143b30558569763480b4384df8c9c9
jruizndev <jorsqn @gmail.com>
2024-09-22 23:57:05 +0200

    botones listos

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import Button from './components/Button'
+import './sass/App.scss'
+import { TiArrowLeftOutline } from 'react-icons/ti'
+import { TiArrowRightOutline } from 'react-icons/ti'
+import { useState } from 'react'
 
 const App = () => {
-    return <div>Hola</div>
+    const [pokemonId, setPokemonId] = useState(1)
+
+    return (
+        <>
+            {/*tarjetas*/}
+            <div className="buttons-container">
+                <Button
+                    icon={<TiArrowLeftOutline />}
+                    handleClick={() => {
+                        setPokemonId(pokemonId - 1)
+                    }}
+                />
+                {pokemonId}
+                <Button
+                    icon={<TiArrowRightOutline />}
+                    handleClick={() => {
+                        setPokemonId(pokemonId + 1)
+                    }}
+                />
+            </div>
+        </>
+    )
 }
 
 export default App
